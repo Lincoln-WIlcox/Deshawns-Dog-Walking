15b6634b5498e916c605e503da1a9ef5881e25c6
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-04-11 14:21:43 -0500

    added radio buttons to filter by city

--- client/src/Walkers.jsx
+++ client/src/Walkers.jsx
@@ -1,23 +1,62 @@
-import { getWalkers } from "./apiManager";
+import { getCities, getWalkers, getWalkersByCity } from "./apiManager";
 import { useEffect, useState } from "react";
 
 const Walkers = () =>
 {
-  const [greeting, setGreeting] = useState({
-    message: "Not Connected to the API",
-  });
+  const [allWalkers, setAllWalkers] = useState([])
+  const [filteredWalkers, setFilteredWalkers] = useState([])
+  const [cities, setCities] = useState([])
+  const [filterCityId, setFilterCityId] = useState(0)
 
-  useEffect(() =>
-  {
-    getWalkers()
-      .then(setGreeting)
-      .catch(() =>
+  useEffect(
+    () =>
+    {
+      getWalkers()
+        .then(setAllWalkers)
+        .catch(() =>
+        {
+          console.log("API not connected")
+        })
+      getCities()
+        .then(setCities)
+    }, [])
+
+  useEffect(
+    () =>
+    {
+      if(filterCityId == 0)
       {
-        console.log("API not connected");
-      });
-  }, []);
+        setFilteredWalkers(allWalkers)
+      } else
+      {
+        getWalkersByCity(filterCityId).then(
+          (walkersByCity) => setFilteredWalkers(walkersByCity)
+        )
+      }
+    }, [filterCityId, allWalkers]
+  )
 
-  return <p>{greeting.message}</p>;
+  return <div>
+    <div>
+      <input type="radio" name="city" value={0} onChange={(event) => setFilterCityId(parseInt(event.target.value))} checked={filterCityId == 0 ? "checked" : ""} />
+      <label htmlFor="city">no filter</label>
+    </div>
+    {
+      cities.map(
+        (city) =>
+          <div key={"city" + city.id}>
+            <input type="radio" name="city" value={city.id} onChange={(event) => setFilterCityId(parseInt(event.target.value))} checked={filterCityId == city.id ? "checked" : ""} />
+            <label htmlFor="city">{city.name}</label>
+          </div>
+      )
+    }
+    {
+      filteredWalkers.map(
+        (walker) =>
+          <p key={"walker" + walker.id}>{walker.name}</p>
+      )
+    }
+  </div>;
 }
 
 export default Walkers
